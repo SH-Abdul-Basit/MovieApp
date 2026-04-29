77faaea0652793d005fff72c09b0bf3b7b1bbf39
Y2kSurviver <abasitmarkting181@gmail.com>
2026-04-29 18:17:25 +0500

    *Addig environment variables

--- src/backend/api.js
+++ src/backend/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "845cad9edf30d5ce741aff2cf0077e8f";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 const getPopularMovies = async () => {
